840700dfe9d976bf14c3475697fd31d74ce32514
Adrian Gidaro <adriangidaro@gmail.com>
2024-11-25 20:02:24 +1100

    Mid lesson

---
+++ callback.py
@@ -0,0 +1,20 @@
+import time
+
+def greet(name, cb):
+    print(f'Hello, {name}!')
+    # time.sleep(3)
+    cb()
+
+
+def say_bye():
+    print('Bye!')
+
+def shout():
+    for i in range(5):
+        print('GOODBYE!!!!!')
+
+# Main
+greet('Steve', say_bye)
+greet('Mary', shout)
+print('Continuing main...')
+# More statements
